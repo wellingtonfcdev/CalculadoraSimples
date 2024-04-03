66c96db0b1a31a811e69586ce702f07f676c98c1
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-03 16:10:13 -0300

    Implementacao das operacoes basicas da matematica

--- Models/Calculadora.cs
+++ Models/Calculadora.cs
@@ -11,5 +11,20 @@ namespace CalculadoraSimples.Models
         {
             Console.WriteLine($"{x} + {y} = {x + y}");
         }
+
+         public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+
+         public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} * {y} = {x * y}");
+        }
+
+          public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CalculadoraSimples.Models;
+
+Calculadora c = new Calculadora();
+
+c.Somar(2,7);
+c.Subtrair(3,9);
+c.Multiplicar(10,2);
+c.Dividir(10,2);
